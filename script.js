60619ad6a086d2e1f3aef0da5e286021d799a65d
Emmanuel Adeleke <aduragbemiemmanuel096@gmail.com>
2026-06-26 02:49:48 -0400

    Add scroll fade-in animation for elements
    Implement scroll fade-in animation using Intersection Observer.

--- script.js
+++ script.js
@@ -106,4 +106,27 @@ filterButtons.forEach(btn => {
     });
 
 });
+   /* =========================
+   SCROLL FADE-IN ANIMATION
+========================= */
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.1
+});
+
+const hiddenElements = document.querySelectorAll(
+    ".project-card, .about-card, .section-title, .skills span, .contact-card"
+);
+
+hiddenElements.forEach(el => observer.observe(el));
 });
